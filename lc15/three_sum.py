7d681b2768beef08992197bfaca8364e99e39e36
Zeke <soto.zeke5@gmail.com>
2023-06-27 21:00:56 -0500

    fixed problem 15 solution

--- lc15/three_sum.py
+++ lc15/three_sum.py
@@ -13,12 +13,17 @@ class Solution(object):
         for index, value in enumerate(nums):
             if value > 0:
                 break
+            if index > 0 and value == nums[index - 1]:
+                continue
+
             start = index + 1
             end = len(nums) - 1
             while start < end:
                 if value + nums[start] + nums[end] == 0:
                     results.append([nums[index], nums[start], nums[end]])
-                    break
+                    start += 1
+                    while nums[start] == nums[start - 1] and start < end:
+                        start += 1
                 elif value + nums[start] + nums[end] > 0:
                     end -= 1
                 else:
