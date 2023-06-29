dcb99533a18e1216f9ad05aa1176a08a1686099c
Zeke <soto.zeke5@gmail.com>
2023-06-29 17:56:45 -0500

    formatting changes

--- lc347/test.py
+++ lc347/test.py
@@ -6,14 +6,15 @@ class TestSolution(unittest.TestCase):
     solution = Solution()
 
     def normal_example(self):
-        input = [1,1,1,2,2,3]
-        correct_output = [1,2]
+        input = [1, 1, 1, 2, 2, 3]
+        correct_output = [1, 2]
         self.assertCountEqual(self.solution.topKFrequent(input, 2), correct_output)
 
     def one_number_example(self):
         input = [1]
         correct_output = [1]
         self.assertCountEqual(self.solution.topKFrequent(input, 1), correct_output)
+
 
 if __name__ == "__main__":
     unittest.main()
