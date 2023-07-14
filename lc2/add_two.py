5d53385095725d9cf9a35209f697168ed7138f3b
Zeke <soto.zeke5@gmail.com>
2023-07-14 18:42:38 -0500

    updated problem 2 after understanding correctly

--- lc2/add_two.py
+++ lc2/add_two.py
@@ -9,39 +9,30 @@ class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         l1_node = l1
         l2_node = l2
+        extra = 0
+        value = 0
+        head = None
         previous_node = None
-        l1_values = []
-        l2_values = []
-        combined_nodes = []
 
         while l1_node or l2_node:
+            value = extra
             if l1_node:
-                l1_values.append(l1_node.val)
+                value += l1_node.val
                 l1_node = l1_node.next
             if l2_node:
-                l2_values.append(l2_node.val)
+                value += l2_node.val
                 l2_node = l2_node.next
-            new_node = ListNode()
+            new_node = ListNode(value % 10)
             if previous_node:
                 previous_node.next = new_node
+            if not head:
+                head = new_node
+            extra = value // 10
             previous_node = new_node
-            combined_nodes.append(new_node)
 
-        extra = 0
-        values = 0
+        if extra > 0:
+            new_node = ListNode(extra)
+            previous_node.next = new_node
 
-        for i in range(1, max(len(l1_values), len(l2_values)) + 1):
-            values = extra
-            if i <= len(l1_values):
-                values += l1_values[-i]
-            if i <= len(l2_values):
-                values += l2_values[-i]
-            extra = values // 10
-            combined_nodes[-i].val = values % 10
 
-        if values < 10:
-            return combined_nodes[0]
-
-        combined_head = ListNode(values // 10, combined_nodes[0])
-        combined_nodes[0].val = values % 10
-        return combined_head
+        return head
