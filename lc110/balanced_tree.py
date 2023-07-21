562047bc934fd0cfce612874ccb2075a0ad3ff3e
Zeke <soto.zeke5@gmail.com>
2023-07-20 19:47:55 -0500

    fixed solution for problem 110

--- lc110/balanced_tree.py
+++ lc110/balanced_tree.py
@@ -8,20 +8,18 @@ class TreeNode:
 
 class Solution:
     def isBalanced(self, root: TreeNode) -> bool:
-        if not root:
-            return True
+        _, balanced = self.isNodeBalanced(root, True)
+        return balanced
 
-        left_edge = self.maxEdge(root.left, 0)
-        right_edge = self.maxEdge(root.right, 0)
-
-        if -1 <= left_edge - right_edge <= 1:
-            return True
-        return False
-
-    def maxEdge(self, node: TreeNode, max_edge: int) -> int:
+    def isNodeBalanced(self, node: TreeNode, balanced: bool):
         if not node:
-            return max_edge
-        return max(
-            self.maxEdge(node.left, max_edge + 1),
-            self.maxEdge(node.right, max_edge + 1),
+            return 0, balanced
+        left_edge, left_balance = self.isNodeBalanced(node.left, balanced)
+        right_edge, right_balance = self.isNodeBalanced(node.right, balanced)
+
+        if left_edge - right_edge < -1 or left_edge - right_edge > 1:
+            balanced = False
+        return (
+            max(left_edge, right_edge) + 1,
+            balanced and left_balance and right_balance,
         )
