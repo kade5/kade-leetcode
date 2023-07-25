25507d43aff6a7aaf3a17ed6c070fb42405879fd
Zeke <soto.zeke5@gmail.com>
2023-07-24 19:03:22 -0500

    fixed solution to problem 98

--- lc98/valid_bst.py
+++ lc98/valid_bst.py
@@ -1,19 +1,23 @@
+import math
+
+
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
-        if not root:
+        return self.isValid(root, -math.inf, math.inf)
+
+    def isValid(self, node, left_bound, right_bound) -> bool:
+        if not node:
             return True
-        
-        left_val = root.left.val if root.left else None
-        right_val = root.right.val if root.right else None
-
-        if (left_val and root.val <= left_val) or (right_val and root.val >= right_val):
+        if node.val <= left_bound or node.val >= right_bound:
             return False
-        
-        return self.isValidBST(root.left) and self.isValidBST(root.right)
-
+        return self.isValid(node.left, left_bound, node.val) and self.isValid(
+            node.right, node.val, right_bound
+        )
