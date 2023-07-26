cb2cb4ec3096ca8d8ef2a50ad0f3baf855709ede
Zeke <soto.zeke5@gmail.com>
2023-07-26 18:47:10 -0500

    initial commit for problem 105

---
+++ lc105/construct_binary_tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def buildTree(self, preorder: list[int], inorder: list[int]) -> TreeNode:
+        if not preorder and not inorder:
+            return None
+
+        node = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        node.left = self.buildTree(preorder[1 : mid + 1], inorder[:mid])
+        node.right = self.buildTree(preorder[mid + 1 :], inorder[mid + 1 :])
+        return node
