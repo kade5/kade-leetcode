e64624c0f216e463b2967ac64dfd4cd07c7784e7
Zeke <soto.zeke5@gmail.com>
2023-07-31 20:29:39 -0500

    new solution to problem 79
    Answer now accepted

--- lc79/word_search.py
+++ lc79/word_search.py
@@ -1,34 +1,31 @@
 class Solution:
     def exist(self, board: list[list[str]], word: str) -> bool:
-
         def findWord(i, j, current_word, pos_set, found) -> bool:
-            if (i, j) in pos_set:
+            if (
+                i < 0
+                or j < 0
+                or i >= len(board)
+                or j >= len(board[0])
+                or (i, j) in pos_set
+                or (len(current_word) > 0 and board[i][j] != current_word[0])
+            ):
                 return found
-            current_word = current_word + board[i][j]
-            if current_word == word:
+            if current_word == board[i][j]:
                 return True
-            
-            if i > 0 and not found:
-                pos_set.add((i, j))
-                found = findWord(i - 1, j, current_word, pos_set, found)
-                pos_set.remove((i, j))
-            if i < len(board) - 1 and not found:
-                pos_set.add((i, j))
-                found = findWord(i + 1, j, current_word, pos_set, found)
-                pos_set.remove((i, j))
-            if j > 0 and not found:
-                pos_set.add((i, j))
-                found = findWord(i, j - 1, current_word, pos_set, found)
-                pos_set.remove((i, j))
-            if j < len(board[0]) - 1 and not found:
-                pos_set.add((i, j))
-                found = findWord(i, j + 1, current_word, pos_set, found)
-                pos_set.remove((i, j))
+            pos_set.add((i, j))
+            if not found:
+                found = findWord(i - 1, j, current_word[1:], pos_set, found)
+            if not found:
+                found = findWord(i + 1, j, current_word[1:], pos_set, found)
+            if not found:
+                found = findWord(i, j - 1, current_word[1:], pos_set, found)
+            if not found:
+                found = findWord(i, j + 1, current_word[1:], pos_set, found)
+            pos_set.remove((i, j))
             return found
-        
+
         for m in range(len(board)):
             for n in range(len(board[0])):
-                if findWord(m, n, "", set(), False):
+                if findWord(m, n, word, set(), False):
                     return True
         return False
-
