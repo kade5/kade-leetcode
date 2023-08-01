4d212ebb87edd84b8a8e50ff999f4dbd703f723f
Zeke <soto.zeke5@gmail.com>
2023-07-31 19:52:23 -0500

    new solution for problem 79
    works but not accepted as time limit exceeded

--- lc79/word_search.py
+++ lc79/word_search.py
@@ -1,34 +1,34 @@
 class Solution:
     def exist(self, board: list[list[str]], word: str) -> bool:
 
-        def findWord(i, j, current_word, pos_set) -> bool:
+        def findWord(i, j, current_word, pos_set, found) -> bool:
+            if (i, j) in pos_set:
+                return found
             current_word = current_word + board[i][j]
             if current_word == word:
                 return True
-            if (i, j) in pos_set:
-                return False
             
-            if i > 0:
+            if i > 0 and not found:
                 pos_set.add((i, j))
-                findWord(i - 1, j, current_word, pos_set)
+                found = findWord(i - 1, j, current_word, pos_set, found)
                 pos_set.remove((i, j))
-            if i < len(board) - 1:
+            if i < len(board) - 1 and not found:
                 pos_set.add((i, j))
-                findWord(i + 1, j, current_word, pos_set)
+                found = findWord(i + 1, j, current_word, pos_set, found)
                 pos_set.remove((i, j))
-            if j > 0:
+            if j > 0 and not found:
                 pos_set.add((i, j))
-                findWord(i, j - 1, current_word, pos_set)
+                found = findWord(i, j - 1, current_word, pos_set, found)
                 pos_set.remove((i, j))
-            if j < len(board[0]) - 1:
+            if j < len(board[0]) - 1 and not found:
                 pos_set.add((i, j))
-                findWord(i, j + 1, current_word, pos_set)
+                found = findWord(i, j + 1, current_word, pos_set, found)
                 pos_set.remove((i, j))
-            return False
+            return found
         
         for m in range(len(board)):
             for n in range(len(board[0])):
-                if findWord(m, n, "", set()):
+                if findWord(m, n, "", set(), False):
                     return True
         return False
 
