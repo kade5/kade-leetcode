bed79976d6a01c7d2d17683c40fccbdeb21749b2
Zeke <soto.zeke5@gmail.com>
2023-08-04 17:08:41 -0500

    alternate solution to problem 215
    Solution that uses quick select. Actual implementation is much slower than using a heap

--- lc215/kth_largest_array.py
+++ lc215/kth_largest_array.py
@@ -1,10 +1,25 @@
-import heapq
-
-
 class Solution:
     def findKthLargest(self, nums: list[int], k: int) -> int:
-        heapq.heapify(nums)
-        while len(nums) > k:
-            heapq.heappop(nums)
+        left = 0
+        right = len(nums) - 1
 
-        return heapq.heappop(nums)
+        while True:
+            val = right
+            part = left
+            for i in range(left, val):
+                if nums[i] <= nums[val]:
+                    swap = nums[part]
+                    nums[part] = nums[i]
+                    nums[i] = swap
+                    part += 1
+
+            swap = nums[val]
+            nums[val] = nums[part]
+            nums[part] = swap
+
+            if part == len(nums) - k:
+                return nums[part]
+            elif len(nums) - k > part:
+                left = part + 1
+            else:
+                right = part - 1
