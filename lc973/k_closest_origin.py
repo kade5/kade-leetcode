d1c055e8998302b91cb76e11c11b2ca530d475d5
Zeke <soto.zeke5@gmail.com>
2023-08-03 19:51:49 -0500

    first pass at solution for problem 973

---
+++ lc973/k_closest_origin.py
@@ -0,0 +1,14 @@
+import math
+import heapq
+
+
+class Solution:
+    def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
+        def distance(point: list[int]):
+            return math.sqrt(point[0] ** 2 + point[1] ** 2)
+
+        distance_list = [(distance(points[i]), i) for i in range(len(points))]
+        heapq.heapify(distance_list)
+
+        result = [points[heapq.heappop(distance_list)[1]] for _ in range(k)]
+        return result
