d27b11edc3b856e6dbeecd3feb13f85402a72264
Zeke <soto.zeke5@gmail.com>
2023-08-13 09:55:22 -0500

    improved solution for problem 684

--- lc684/redundant_connection.py
+++ lc684/redundant_connection.py
@@ -1,16 +1,33 @@
 class Solution:
     def findRedundantConnection(self, edges: list[list[int]]) -> list[int]:
-        trees = [{node} for node in range(1, len(edges) + 1)]
+        parent = [node for node in range(len(edges) + 1)]
+        rank = [1] * (len(edges) + 1)
+
+        def findParent(n):
+            p = parent[n]
+
+            while p != parent[p]:
+                parent[p] = parent[parent[p]]
+                p = parent[p]
+            return p
+
+        def union(n1, n2):
+            p1, p2 = findParent(n1), findParent(n2)
+
+            if p1 == p2:
+                return False
+            
+            if rank[p1] > rank[p2]:
+                parent[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+
+            return True
 
         for pair in edges:
-            temp_set = set()
-            for i in range(len(trees)):
-                if pair[0] in trees[i] and pair[1] in trees[i]:
-                    return pair
-                if not temp_set and (pair[0] in trees[i] or pair[1] in trees[i]):
-                    temp_set = trees[i]
-                    trees[i] = set()
-                elif pair[1] in trees[i] or pair[0] in trees[i]:
-                    trees[i] = trees[i].union(temp_set)
+            if not union(pair[0], pair[1]):
+                return pair
 
         return []
