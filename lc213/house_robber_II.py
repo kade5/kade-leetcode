6bbf6f256ba26d17da543ffd00ba7d1d47027c20
Zeke <soto.zeke5@gmail.com>
2023-08-17 17:57:17 -0500

    improved solution for problem 213

--- lc213/house_robber_II.py
+++ lc213/house_robber_II.py
@@ -1,19 +1,17 @@
 class Solution:
     def rob(self, nums: list[int]) -> int:
-        memory = {}
-
         def robRange(s, e):
-            if (s, e) in memory:
-                return memory[(s, e)]
+            rob1, rob2 = 0, 0
 
-            if s >= len(nums) or e >= len(nums) or s > e:
-                return 0
+            while s <= e:
+                temp = max(nums[s] + rob1, rob2)
+                rob1 = rob2
+                rob2 = temp
+                s += 1
 
-            if s == e - 1:
-                memory[(s, e)] = max(nums[s], nums[e])
-                return memory[(s, e)]
+            return rob2
 
-            memory[(s, e)] = max(nums[s] + robRange(s + 2, e), robRange(s + 1, e))
-            return memory[(s, e)]
+        if len(nums) == 1:
+            return nums[0]
 
-        return max(nums[0] + robRange(2, len(nums) - 2), robRange(1, len(nums) - 1))
+        return max(robRange(0, len(nums) - 2), robRange(1, len(nums) - 1))
