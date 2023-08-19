a7866b63827256a4245c5845d2b3235d8af8e336
Zeke <soto.zeke5@gmail.com>
2023-08-19 18:12:35 -0500

    initial commit for problem 139
    appears to work but time limit exceeded

---
+++ lc139/word_break.py
@@ -0,0 +1,44 @@
+class Trie:
+    def __init__(self):
+        self.next = {}
+        self.word = False
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: list[str]) -> bool:
+        trie = self.buildTrie(wordDict)
+
+        def search(s):
+            current_trie = trie
+            i = 0
+            found = False
+            while i < len(s) and not found:
+                if current_trie.word:
+                    found = search(s[i:])
+                if s[i] not in current_trie.next:
+                    break
+                current_trie = current_trie.next[s[i]]
+                i += 1
+
+            if i == len(s) and current_trie.word:
+                return True
+            return found
+
+        return search(s)
+
+    def buildTrie(self, wordDict):
+        head = Trie()
+        currentTrie = head
+
+        for word in wordDict:
+            currentTrie = head
+            for letter in word:
+                if letter in currentTrie.next:
+                    currentTrie = currentTrie.next[letter]
+                else:
+                    currentTrie.next[letter] = Trie()
+                    currentTrie = currentTrie.next[letter]
+
+            currentTrie.word = True
+
+        return head
