c25eb475fb748882173d38eadb5e2d249a8e90af
Zeke <soto.zeke5@gmail.com>
2023-08-19 22:05:06 -0500

    fixed solution for problem 300
    Solution accepted but still very slow and not nlogn as requested

--- lc300/long_inc_subseq.py
+++ lc300/long_inc_subseq.py
@@ -2,22 +2,11 @@ class Solution:
     def lengthOfLIS(self, nums: list[int]) -> int:
         cache = {len(nums) - 1: 1}
 
-        def search(pos):
-            if pos in cache:
-                return cache[pos]
-            i = pos
-
-            while i + 1 < len(nums) and nums[i + 1] <= nums[pos]:
-                i += 1
-
-            if i + 1 < len(nums):
-                cache[pos] = 1 + search(i + 1)
-                return cache[pos]
-
-            cache[pos] = 1
-            return cache[pos]
-
-        for i in range(len(nums) - 1, -1, -1):
-            search(i)
+        for i in range(len(nums) - 2, -1, -1):
+            maximum = 1
+            for j in range(i + 1, len(nums)):
+                if nums[j] > nums[i]:
+                    maximum = max(maximum, 1 + cache[j])
+                cache[i] = maximum
 
         return max(cache.values())
