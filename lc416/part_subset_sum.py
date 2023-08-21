fceabaed63f37d75257895756f66412bd5b0a4a9
Zeke <soto.zeke5@gmail.com>
2023-08-20 19:15:37 -0500

    improved solution for problem 416

--- lc416/part_subset_sum.py
+++ lc416/part_subset_sum.py
@@ -5,23 +5,16 @@ class Solution:
             return False
         sum_nums = sum_nums // 2
 
-        if nums[len(nums) - 1] == sum_nums:
-            return True
+        cache = set()
+        cache.add(0)
 
-        cache = [set()] * len(nums)
-        cache[len(nums) - 1].add(nums[len(nums) - 1])
-
-        for i in range(len(nums) - 2, -1, -1):
+        for i in range(len(nums) - 1, -1, -1):
             new_set = set()
-            if nums[i] == sum_nums:
-                return True
-            for s in cache[i + 1]:
+            for s in cache:
                 if nums[i] + s == sum_nums:
                     return True
                 if nums[i] + s < sum_nums:
                     new_set.add(nums[i] + s)
-            if nums[i] < sum_nums:
-                cache[i].add(nums[i])
-            cache[i + 1].update(new_set)
+            cache.update(new_set)
 
         return False
