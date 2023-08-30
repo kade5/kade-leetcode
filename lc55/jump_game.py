fc639a5a969ca6add84136d9e9e77a0b84e44da3
Zeke Soto <ssz.kade@gmail.com>
2023-08-29 20:24:35 -0500

    improved solution for problem 55

--- lc55/jump_game.py
+++ lc55/jump_game.py
@@ -1,14 +1,9 @@
 class Solution:
     def canJump(self, nums: list[int]) -> bool:
-        can_jump = set()
+        goal = len(nums) - 1
 
         for i in range(len(nums) - 1, -1, -1):
-            if nums[i] - (len(nums) - i - 1) >= 0:
-                can_jump.add(i)
-            else:
-                for j in range(nums[i], 0, -1):
-                    if j + i in can_jump:
-                        can_jump.add(i)
-                        break
+            if i + nums[i] >= goal:
+                goal = i
 
-        return 0 in can_jump
+        return goal == 0 
