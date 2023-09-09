253262875aa90c5ad6b48a14f7c6945d977dbe4c
Zeke <soto.zeke5@gmail.com>
2023-09-09 15:07:43 -0500

    third try at solution to problem 678
    answer finally correct

--- lc678/valid_parenthesis.py
+++ lc678/valid_parenthesis.py
@@ -1,23 +1,22 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        left_count = 0
-        right_count = 0
-        star_count = 0
+        left_max_count = 0
+        left_min_count = 0
         for c in s:
             if c == "(":
-                left_count += 1
-            if c == ")":
-                right_count += 1
-            if c == "*":
-                star_count += 1
+                left_max_count += 1
+                left_min_count += 1
+            elif c == ")":
+                left_max_count -= 1
+                left_min_count -= 1
+            else:
+                left_max_count += 1
+                left_min_count -= 1
 
-            if (
-                left_count < right_count + star_count
-                and left_count + star_count < right_count
-            ):
+            if left_max_count < 0:
                 return False
 
-        if abs(left_count - right_count) > star_count:
-            return False
+            if left_min_count < 0:
+                left_min_count = 0
 
-        return True
+        return left_min_count <= 0 <= left_max_count
