bd41d0aedbbb76c718f77eb70318c82d5b7556b0
Zeke <soto.zeke5@gmail.com>
2023-09-16 17:38:30 -0500

    improved solution to problem 62

--- lc62/unique_paths.py
+++ lc62/unique_paths.py
@@ -1,21 +1,10 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memory = {}
-        memory[(m - 1, n - 1)] = 1
+        memory = [[1] * n for _ in range(m)]
 
-        def dfs(i, j):
-            nonlocal m, n
-            sum1, sum2 = 0, 0
-            if (i, j) in memory:
-                return memory[(i, j)]
+        for i in range(1, m):
+            for j in range(1, n):
+                memory[i][j] = memory[i - 1][j] + memory[i][j - 1]
 
-            if i < m:
-                sum1 = dfs(i + 1, j)
+        return memory[m - 1][n - 1]
 
-            if j < n:
-                sum2 = dfs(i, j + 1)
-
-            memory[(i, j)] = sum1 + sum2
-            return memory[(i, j)]
-
-        return dfs(0, 0)
