a1056a71798e065713bbfab59fa319d6aed18e7e
Zeke <soto.zeke5@gmail.com>
2023-09-18 18:59:48 -0500

    fixed solution to problem 518

--- lc518/coin_change_II.py
+++ lc518/coin_change_II.py
@@ -1,26 +1,18 @@
 class Solution:
     def change(self, amount: int, coins: list[int]) -> int:
-        cache = set()
-        coin_amount = [0 * len(coins)]
-        result = 0
+        cache = [[1] * (amount + 1) for _ in range(len(coins))]
 
-        def dfs(coin_amount: list[int], coin_total):
-            nonlocal result
-            if coin_total > amount or coin_amount in cache:
-                return
+        for c in range(len(coins) - 1, -1, -1):
+            for a in range(1, amount + 1):
+                coin_amount = a - coins[c]
+                if coin_amount < 0:
+                    with_coin = 0
+                else:
+                    with_coin = cache[c][coin_amount]
+                if c + 1 >= len(coins):
+                    without_coin = 0
+                else:
+                    without_coin = cache[c + 1][a]
+                cache[c][a] = with_coin + without_coin
 
-            if coin_total == amount:
-                result += 1
-                return
-
-            cache.add(coin_amount)
-
-            for i in range(len(coins)):
-                coin = coins[i]
-                coin_amount[i] += 1
-                dfs(coin_amount, coin + coin_total)
-
-        dfs(coin_amount, 0)
-        return result
-
-        
+        return cache[0][amount]
