21b25d3c07b29a24513ccdc3516df2cee8feeae1
Zeke Soto <ssz.kade@gmail.com>
2023-10-17 17:27:14 -0500

    fixed solution to problem 1361

--- lc1361/valid_binary_tree.py
+++ lc1361/valid_binary_tree.py
@@ -5,9 +5,12 @@ class Solution:
     def validateBinaryTreeNodes(
         self, n: int, leftChild: list[int], rightChild: list[int]
     ) -> bool:
-        visited = set()
+        head = self.findHead(n, leftChild, rightChild)
+        if head == -1:
+            return False
         queue = deque()
-        queue.append(0)
+        queue.append(head)
+        visited = set()
 
         while queue:
             cur = queue.popleft()
@@ -23,3 +26,20 @@ class Solution:
                 queue.append(rightChild[cur])
 
         return len(visited) == n
+
+    def findHead(self, n: int, leftChild: list[int], rightChild: list[int]) -> int:
+        visited = set(range(n))
+
+        for i in range(n):
+            if leftChild[i] != -1:
+                if leftChild[i] not in visited:
+                    return -1
+                visited.remove(leftChild[i])
+            if rightChild[i] != -1:
+                if rightChild[i] not in visited:
+                    return -1
+                visited.remove(rightChild[i])
+
+        if len(visited) != 1:
+            return -1
+        return list(visited)[0]
