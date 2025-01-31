71b0c56e44f53e57bcd25f13836d053a5c7e4490
Zeke <soto.zeke5@gmail.com>
2025-01-30 19:17:34 -0600

    fixed lc1730 to search from starting point instead of food

--- lc1730/shortest_path_food.py
+++ lc1730/shortest_path_food.py
@@ -4,38 +4,30 @@ from collections import deque
 class Solution:
     def getFood(self, grid: list[list[str]]) -> int:
         person = (0, 0)
-        food_list = []
         m = len(grid)
         n = len(grid[0])
-        solution_grid = [[m * n for _ in range(n)] for _ in range(m)]
         directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
         for (i, row) in enumerate(grid):
             for (j, c) in enumerate(row):
                 if c == '*':
                     person = (i, j)
-                elif c == '#':
-                    food_list.append((i, j))
+                    break
 
-        for food in food_list:
-            visited = set()
-            qu = deque()
-            qu.append((food, 0))
-            while qu:
-                pos, steps = qu.popleft()
-                visited.add(pos)
-                if solution_grid[pos[0]][pos[1]] > steps:
-                    solution_grid[pos[0]][pos[1]] = steps
-                for direct in directions:
-                    if (0 <= pos[0] + direct[0] < m
-                        and 0 <= pos[1] + direct[1] < n
-                        and (pos[0] + direct[0], pos[1] + direct[1]) not in visited
-                        and grid[pos[0] + direct[0]][pos[1] + direct[1]] != 'X'
-                        and grid[pos[0] + direct[0]][pos[1] + direct[1]] != '#')\
-                        and solution_grid[pos[0] + direct[0]][pos[1] + direct[1]] > steps + 1:
-                        qu.append(((pos[0] + direct[0], pos[1] + direct[1]), steps + 1))
-
-        if solution_grid[person[0]][person[1]] < m * n:
-            return solution_grid[person[0]][person[1]]
+        visited = set()
+        qu = deque()
+        qu.append((person, 0))
+        visited.add(person)
+        while qu:
+            pos, steps = qu.popleft()
+            if grid[pos[0]][pos[1]] == '#':
+                return steps
+            for direct in directions:
+                if (0 <= pos[0] + direct[0] < m
+                    and 0 <= pos[1] + direct[1] < n
+                    and (pos[0] + direct[0], pos[1] + direct[1]) not in visited
+                    and grid[pos[0] + direct[0]][pos[1] + direct[1]] != 'X'):
+                    qu.append(((pos[0] + direct[0], pos[1] + direct[1]), steps + 1))
+                    visited.add((pos[0] + direct[0], pos[1] + direct[1]))
 
         return -1
